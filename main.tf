147370f527905e5d63b57aff7399c9b47ef04eb7
Przemek Tadeusz <cloudedtalesblog@gmail.com>
2021-10-14 00:56:05 +0200

    modules

--- main.tf
+++ main.tf
@@ -13,38 +13,7 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-data "aws_vpc" "default" {
-  default = true
-} 
-
-data "aws_subnet_ids" "destination" {
-  vpc_id = data.aws_vpc.default.id
-  filter {
-      name = "availability-zone"
-      values = ["eu-west-1a"]
-  }
-}
-
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-    owners = ["099720109477"]
-}
-
-resource "aws_instance" "web" {
-  for_each      = data.aws_subnet_ids.destination.ids
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id = each.value
-  associate_public_ip_address = true
-  tags = {
-    Name = "HelloWorld"
-  }
+module "ec2" {
+  source = "./modules/ec2"
+  name = "HelloWorld"
 }
